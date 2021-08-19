02603b9700ea3d32213c0f9eb45daea84c3bc59d
Anastacio <rodrigo.anastacio@wundermanthompson.com>
2021-08-19 15:31:45 -0300

    Animação no Hero

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 import Header from '../components/Header';
 import Headline from '../components/Headline';
@@ -19,26 +20,42 @@ const Home = () => {
       <section className={styles.hero}>
         <div className={styles.container}>
           <div className={styles.wrapper}>
-            <Headline
-              title="Marketing descomplicado e sem fórmulas prontas"
-              subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
-              perspiciatis assumenda minima ducimus iure fuga, aliquam tempora autem
-              vero quaerat inventore dolorum ullam eos, dolor reiciendis modi
-              voluptatibus dolore distinctio?"
-            />
+            <div className={styles.text}>
+              <motion.div
+                initial={{ x: -60, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ delay: 0.4 }}
+              >
+                <Headline
+                  title="Marketing descomplicado e sem fórmulas prontas"
+                  subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+                  perspiciatis assumenda minima ducimus iure fuga, aliquam tempora autem
+                  vero quaerat inventore dolorum ullam eos, dolor reiciendis modi
+                  voluptatibus dolore distinctio?"
+                ></Headline>
+              </motion.div>
+            </div>
 
             <div className={styles.image}>
-              <img className="shape" src="./images/borboleta.png" alt="" />
-              <img className="shape2" src="./images/borboleta2.png" alt="" />
-              <img className="shape3" src="./images/borboleta3.png" alt="" />
-
-              <Image
-                className={styles.perfil}
+              <motion.div
                 src="/images/dayane-silva-hero.png"
-                width={426}
-                height={376}
-                alt="Imagem de perfil"
-              />
+                alt="Imagem que ilustra a consultora Dayane Silva"
+                initial={{ y: 60, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ delay: 0.2 }}
+              >
+                <img className="shape" src="./images/borboleta.png" alt="" />
+                <img className="shape2" src="./images/borboleta2.png" alt="" />
+                <img className="shape3" src="./images/borboleta3.png" alt="" />
+
+                <Image
+                  className={styles.perfil}
+                  src="/images/dayane-silva-hero.png"
+                  width={426}
+                  height={376}
+                  alt="Imagem de perfil"
+                />
+              </motion.div>
             </div>
           </div>
         </div>
